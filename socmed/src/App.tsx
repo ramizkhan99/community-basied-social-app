c6da908af67e0ee4e17d71e0adbe5b47aa6df2f6
Ramiz Khan <ramizkhan1199@gmail.com>
2020-09-05 19:05:50 +0530

    Skeleton ready for departure

--- socmed/src/App.tsx
+++ socmed/src/App.tsx
@@ -3,18 +3,12 @@ import React from "react";
 import "./App.scss";
 
 // All component calls here
-import Button from "./components/Button";  
-import Editor from "./components/editor"         //test remove later
-
-
+import Button from "./components/Button";
 
 function App() {
-
-    
     return (
         <div className="App">
             <h1>Socmed</h1>
-            <Editor/>
             <Button name="Mushi Mushi" />
         </div>
     );
---
+++ socmed/src/components/Editor.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+function Editor() {
+    return <ReactQuill theme="snow" />;
+}
+
+export default Editor;
