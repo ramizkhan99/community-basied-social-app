04b72da3a53d707c6047add1ce548c6b7bb06eb8
Ramiz Khan <ramizkhan1199@gmail.com>
2020-09-29 21:49:11 +0530

    Merge with master

--- socmed/src/App.tsx
+++ socmed/src/App.tsx
@@ -1,34 +1,26 @@
 import React from "react";
 import "./App.scss";
-import { BrowserRouter,Switch,Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // All component calls here
 
-import SignIn from "./components/login/login";
+import Login from "./components/login/login";
 import SignUp from "./components/signup/signup";
-import Button from "./components/layout/Button";
-import { signOut, signUp } from "./store/actions/userActions";
-import Dashboard from "./components/layout/Dashboard"
-import Editor from "./components/layout/Editor"
-
-
+import Dashboard from "./components/layout/Dashboard";
+import Editor from "./components/layout/Editor";
 
 function App() {
     return (
-        
-    <BrowserRouter>
-      <div className="App">
-        
-        <Switch>
-          <Route exact path='/' component={Dashboard}/>
-          <Route path='/signin' component={SignIn} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/post'   component={Editor}/>
-        </Switch>
-        
-        
-      </div>
-    </BrowserRouter>
+        <BrowserRouter>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/post" component={Editor} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
