282723198c548f0bcdb198c7f6715148ecb2c14c
Ramiz Khan <ramizkhan1199@gmail.com>
2020-10-03 19:43:09 +0530

    Fixed cookies on backend

--- socmed/src/store/actions/userActions.tsx
+++ socmed/src/store/actions/userActions.tsx
@@ -17,51 +17,55 @@ export const signIn = (credentials: ICredentials) => {
     //console.log(credentials);
     return (dispatch: any, getState: any) => {
         axios
-            .post("http://localhost:5000/login", {
-                username: credentials.username,
-                password: credentials.password
-            })
-            .then((res:any) => {
+            .post(
+                "http://localhost:3980/login",
+                {
+                    username: credentials.username,
+                    password: credentials.password,
+                },
+                { withCredentials: true }
+            )
+            .then((res: any) => {
                 console.log(res);
                 dispatch({
                     type: "LOGIN_SUCCESS",
                     userId: res.data.id,
                     token: res.data.token,
-                    username:res.data.username
+                    username: res.data.username,
                 });
             })
             .catch((err) => {
-               // console.log(err);
+                // console.log(err);
                 dispatch({ type: "LOGIN_ERROR", err });
             });
     };
 };
 
 export const signOut = () => {
-    
     return (dispatch: any, getState: any) => {
-        
-        axios.post("http://localhost:5000/signout",{
-            
-        },{
-            headers:{
-                'authorization': 'Bearer '+ getState().userReducer.token
-            }
-        }).then(() => {
-            dispatch({ type: "SIGNOUT_SUCCESS" });
-        });
+        axios
+            .get("http://localhost:3980/signout", { withCredentials: true })
+            .then(() => {
+                dispatch({ type: "SIGNOUT_SUCCESS" });
+            });
     };
 };
 
 export const signUp = (newUser: INewUser) => {
     return (dispatch: any, getState: any) => {
         axios
-            .post("http://localhost:5000/users", {
-                username: newUser.username,
-                password: newUser.password,
-                email: newUser.email,
-                firstName: newUser.firstName
-            })
+            .post(
+                "http://localhost:3980/users",
+                {
+                    username: newUser.username,
+                    password: newUser.password,
+                    email: newUser.email,
+                    firstName: newUser.firstName,
+                },
+                {
+                    withCredentials: true,
+                }
+            )
             .then(() => {
                 dispatch({ type: "SIGNUP_SUCCESS" });
             })
