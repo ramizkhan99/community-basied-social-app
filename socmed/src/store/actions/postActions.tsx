46574c6a502abe49620d813d2146b91bbe473b3b
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2020-10-07 12:02:52 +0530

    added post reducer and action

---
+++ socmed/src/store/actions/postActions.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export interface INewPost {
+    title:String;
+    content:String;
+    genre:String;
+
+}
+
+export const newPost = (postDetails:INewPost)=> {
+    return (dispatch: any, getState: any) => {
+        const authorId = getState().userReducer.user
+        const authorName = getState().userReducer.username
+        console.log("inside post actions",authorId,authorName,postDetails)
+        
+        axios
+            .post(
+                "http://localhost:5000/users",
+                {
+                    authorId:authorId,
+                    authorName:authorName,
+                    title:postDetails.title,
+                    content:postDetails.content,
+                    genre:postDetails.genre,
+                    
+                },
+                {
+                    withCredentials: true,
+                }
+            )
+            .then(() => {
+                dispatch({ type: "CREATE_POST",postDetails});
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch({ type: "CREATE_POST_ERROR", err });
+            });
+    };
+};
---
+++ socmed/src/store/reducers/postReducer.js
@@ -0,0 +1,24 @@
+const initState = {
+    posts:[
+        {id:'1',title:'title here',content:'content here',genre:'genre here'},
+        {id:'2',title:'title here',content:'content here',genre:'genre here'},
+        {id:'3',title:'title here',content:'content here',genre:'genre here'}
+
+    ]
+}
+
+const projectReducer = (state=initState,action)=>{
+    switch(action.type){
+        case 'CREATE_POST':
+            console.log('post created',action.project);
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('Error occured while posting',action.err);
+            return state;
+        default:
+            return state;
+
+    }
+}
+
+export default projectReducer
